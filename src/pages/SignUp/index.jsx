2165e83ee1fcbc683dd069acaef43b0a880391a6
Guilherme Totti <dev.guilhermetotti@gmail.com>
2023-11-29 21:10:36 -0300

    ✨ feat: backend integration: user sign-up

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,11 +1,39 @@
+import { useState } from 'react';
 import { FiMail, FiLock, FiUser } from 'react-icons/fi';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
+import { api } from '../../service/api';
+ 
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
 import { Container, Form, Background } from './styles';
 
 export function SignUp() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  
+  const navigate = useNavigate();
+
+  function handleSignUp() {
+    if(!name || !email || !password) {
+      return alert("Preencha todos os campos.");
+    }
+
+    api.post("/users", { name, email, password })
+      .then(() => {
+        alert("Usuário cadastrado com sucesso!");
+        navigate("/");
+      })
+      .catch(error => {
+        if(error.response){
+          return alert(error.response.data.message);
+        } else {
+          alert("Não foi possível realizar cadastro de usuário.");
+        }
+      });    
+  }
+
   return(
     <Container>
       <Background/>
@@ -20,21 +48,24 @@ export function SignUp() {
           placeholder="Nome"
           type="text"
           icon={FiUser}
+          onChange={e => setName(e.target.value)}
         />
 
         <Input 
           placeholder="E-mail"
           type="text"
           icon={FiMail}
+          onChange={e => setEmail(e.target.value)}
         />
 
         <Input 
           placeholder="Senha"
           type="password"
           icon={FiLock}
+          onChange={e => setPassword(e.target.value)}
         />
 
-        <Button title="Cadastrar"/>
+        <Button title="Cadastrar" onClick={handleSignUp}/>
 
         <Link to="/">
           Voltar para o login
