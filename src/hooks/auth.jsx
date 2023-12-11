e395b02d84ad954dee261e6d900da0fdb6ba182c
Guilherme Totti <dev.guilhermetotti@gmail.com>
2023-12-11 16:42:05 -0300

    ✨ feat: logout function implementation

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -28,6 +28,13 @@ function AuthProvider({ children }) {
     }
   }
 
+  function signOut() {
+    localStorage.removeItem("@rocketnotes:user");
+    localStorage.removeItem("@rocketnotes:token");
+
+    setData({});
+  }
+
   useEffect(() => {
     const user = localStorage.getItem("@rocketnotes:user");
     const token = localStorage.getItem("@rocketnotes:token");
@@ -44,7 +51,11 @@ function AuthProvider({ children }) {
   }, []);
 
   return(
-    <AuthContext.Provider value={{ signIn, user: data.user }}>
+    <AuthContext.Provider value={{ 
+      signIn,
+      signOut, 
+      user: data.user
+    }}>
       {children}
     </AuthContext.Provider>
   );
