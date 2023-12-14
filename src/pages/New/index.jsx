0887baaf91d701afbbe3b4cf1300a61a6e47d5f1
Guilherme Totti <dev.guilhermetotti@gmail.com>
2023-12-14 16:36:58 -0300

    ✨ feat: add and remove links to a note

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import { Header } from '../../components/Header';
@@ -10,6 +11,18 @@ import { Section } from '../../components/Section';
 import { Container, Form } from './styles';
 
 export function New() {
+  const [links, setLinks] = useState([]);
+  const [newLink, setNewLink] = useState("");
+
+  function handleAddLink() {
+    setLinks(prevState => [...prevState, newLink]);
+    setNewLink("");
+  }
+
+  function handleRemoveLink(deleted) {
+    setLinks(prevState => prevState.filter(link => link !== deleted))
+  }
+
   return(
     <Container>
       <Header />
@@ -25,8 +38,22 @@ export function New() {
           <TextArea placeholder="Observações"/>
 
           <Section title="Links úteis">
-            <NoteItem value="https://rocketseat.com.br" />
-            <NoteItem isNew placeholder="Novo link"/>
+            {
+              links.map((link, index) => (
+                <NoteItem 
+                  key={String(index)}
+                  value={link}
+                  onClick={() => handleRemoveLink(link)} 
+                />
+              ))
+            }
+            <NoteItem 
+              isNew 
+              placeholder="Novo link"
+              value={newLink}
+              onChange={e => setNewLink(e.target.value)}
+              onClick={handleAddLink} 
+            />
           </Section>
 
           <Section title="Marcadores">
